0f447ac51dad55301c79d15e85ffafaa826b69d0
[dyad] <tufonoayuda@gmail.com>
2025-12-24 20:08:32 -0300

    [dyad] wrote 1 file(s)

--- src/pages/VoiceLabPage.tsx
+++ src/pages/VoiceLabPage.tsx
@@ -317,7 +317,7 @@ export default function VoiceLabPage() {
         <PitchVisualizer pitch={pitch} pitchHistory={pitchHistory} />
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6"> {/* Cambiado de lg:grid-cols-2 a md:grid-cols-2 */}
         <Card>
           <CardHeader>
             <CardTitle>Métricas Vocales</CardTitle>
